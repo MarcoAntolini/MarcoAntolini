c71d8fcffe196d0078bbaa5c2b7847cb967b4c57
MarcoAntolini <marco.antolini2001@gmail.com>
2023-10-27 02:15:35 +0200

    update rewrites configuration

--- next.config.js
+++ next.config.js
@@ -1,4 +1,25 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+	async rewrites() {
+		return [
+			{
+				source: "/next-app-template",
+				destination: "https://next-app-template-phi.vercel.app",
+			},
+			{
+				source: "/next-app-template/:path*",
+				destination: "https://next-app-template-phi.vercel.app/:path*",
+			},
+			{
+				source: "/dso-database",
+				destination: "https://dso-database-dun.vercel.app",
+			},
+			{
+				source: "/dso-database/:path*",
+				destination: "https://dso-database-dun.vercel.app/:path*",
+			},
+		];
+	},
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
