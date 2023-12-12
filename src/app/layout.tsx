f0a527a00d91c1921bd3dfe9e17c7b86f36923b8
MarcoAntolini <marco.antolini2001@gmail.com>
2023-12-12 19:02:31 +0100

    update favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,11 +12,17 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
 	title: "Marco Antolini | Personal Portfolio",
 	description: "Marco Antolini personal website.",
+	icons: {
+		icon: "https://marcoantolini.com/favicon.ico",
+	},
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
 		<html lang="en" className="!scroll-smooth">
+			<head>
+				<link rel="icon" href="https://marcoantolini.com/favicon.ico" />
+			</head>
 			<body
 				className={`${inter.className} bg-gray-50 text-gray-950 relative pt-28 sm:pt-36 dark:bg-gray-900 dark:text-gray-50 dark:text-opacity-90`}
 			>
